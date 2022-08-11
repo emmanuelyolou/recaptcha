d4f0f1b75f12ea21d16b0ca0b45aade45b3058cb
Emmanuel Yolou <emmanuelyolou@gmail.com>
2022-08-11 08:41:56 +0000

    implémentation du recaptcha

--- public/app.js
+++ public/app.js
@@ -1,16 +1,17 @@
-function enableRecaptcha(e) {
+document.querySelector('.submit').addEventListener('click', submitForm);
+
+const firebaseKey = '6LdrCGUhAAAAAMjcnina0bHg4tEttexgtlgj1tcE';
+const localKey = '6Lfr1mQhAAAAAI4kRKYeCtY8QfmtznPpi6eiUgqm';
+
+function submitForm(e) {
   e.preventDefault();
   grecaptcha.ready(function() {
-    grecaptcha.execute('reCAPTCHA_site_key', {action: 'submit'}).then(function(token) {
+    grecaptcha.execute(localKey, {action: 'submit'}).then(function(token) {
         // Add your logic to submit to your backend server here.
+        console.log(token);
     });
   });
 }
 
-function onSubmit(token) {
-  document.getElementById("demo-form").submit();
-}
-const firebaseKey = '6LdrCGUhAAAAAMjcnina0bHg4tEttexgtlgj1tcE';
-const localKey = '6Lfr1mQhAAAAAI4kRKYeCtY8QfmtznPpi6eiUgqm';
 
 // document.querySelector(".submit").addEventListener("click", function (){  console.log("fuck")});
